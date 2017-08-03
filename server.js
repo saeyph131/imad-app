dbcbd382ddbcc6d5afacf255b4edd776fc546157
saeyph131 <30487414+saeyph131@users.noreply.github.com>
2017-08-03 20:26:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-  
+
+app.get('/ui/BIG_google_wave_logo.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'BIG_google_wave_logo.jpg'));
+    
 
 });
 
