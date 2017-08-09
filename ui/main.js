cb560ccba4aacde34359c8471457ffaaca741249
saeyph131 <30487414+saeyph131@users.noreply.github.com>
2017-08-09 22:56:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,29 @@
 //counter
-var counter=0;
+
 var button=document.getElementById('counter');
 
 
 button.onclick=function(){
     
   //request
-  
+  var request = new XMLHttpRequest();
   //response
+  request.onreadystatechange = function(){
+      
+    if(request.readyState === XMLHttpRequest.DONE){
+        
+        if(request.status === 200){
+            var counter=request.responseText;
+              var span=document.getElementById('count');
+  span.innerHTML=counter.toString();
+        }
+    }    
+      
+  };
+  
   
   //render var
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter.toString();
+
+  request.open('GET','http://ahsaifullah131.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
