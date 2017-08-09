4b72d5b8f49c11c8d0a0f2470f73ceaa2f3fdaa0
saeyph131 <30487414+saeyph131@users.noreply.github.com>
2017-08-09 22:35:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,12 @@ var htmlTemplate= `
 }
 
 
+var counter=0;
+
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 
 
 app.get('/', function (req, res) {
